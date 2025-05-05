54fab3a1807ba36183249bf8f60a490f5d7444eb
Psych-DF <95335322+Psych-DF@users.noreply.github.com>
2025-05-05 17:23:43 +1000

    Update player.js

--- js/player.js
+++ js/player.js
@@ -1,11 +1,15 @@
-// js/player.js
+/* POSTION LOGIC */
 export const player = {
   x: 250, // middle of 150
   y: 250,
   spawnX: 250,
   spawnY: 250,
+/* STEPS LOGIC */
   maxSteps: 50,
   stepsLeft: 50,
+/* DIG COUNTER */
+  digs: 0,
+/* ITEMS / RESOURCE COUNTERS*/  
   diamond: 0,
   gold: 0,
   silver: 0,
